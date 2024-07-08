a1da371d4cff65af089482a4045aae4c5e30aaa0
vuongquangminh <vuongquangminh120802@gmail.com>
2024-07-08 22:19:28 +0700

    migrate table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->string('phone', 20);
+            $table->string('address')->nullable();
+            $table->string('avatar', 255)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
