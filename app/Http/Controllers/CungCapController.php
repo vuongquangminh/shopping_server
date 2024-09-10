37bb181810ff06994087cba0d349c33203948167
vuongquangminh <vuongquangminh120802@gmail.com>
2024-09-10 22:52:23 +0700

    feat: order by

--- app/Http/Controllers/CungCapController.php
+++ app/Http/Controllers/CungCapController.php
@@ -12,32 +12,32 @@ class CungCapController extends Controller
 {
     public function customersAll()
     {
-        $query = User::query()->where('role_name', 'customer');
+        $query = User::query()->where('role_name', 'customer')->orderBy('id');
         return response()->json($query->get());
     }
     public function nhanSuAll()
     {
-        $query = User::query()->where('role_name', 'nhan_su');
+        $query = User::query()->where('role_name', 'nhan_su')->orderBy('id');
         return response()->json($query->get());
     }
 
     public function chips(Request $request)
     {
         $type_product_id = $request->input('type_product_id');
-        $query = Chip::query()->where('type_product_id', $type_product_id);
+        $query = Chip::query()->where('type_product_id', $type_product_id)->orderBy('id');
         return response()->json($query->get());
     }
 
     public function mauSacs(Request $request)
     {
         $type_product_id = $request->input('type_product_id');
-        $query = MauSac::query()->where('type_product_id', $type_product_id);
+        $query = MauSac::query()->where('type_product_id', $type_product_id)->orderBy('id');
         return response()->json($query->get());
     }
 
     public function dungLuongs()
     {
-        $query = DungLuong::query();
+        $query = DungLuong::query()->orderBy('id');
         return response()->json($query->get());
     }
 }
